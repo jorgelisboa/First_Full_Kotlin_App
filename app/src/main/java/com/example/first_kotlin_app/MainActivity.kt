8c9cf39b1f51d25a258c76b55243f3123add2f39
Jorge Lisboa <jorgelisboa659@gmail.com>
2021-07-19 00:41:52 -0300

    splash screen done

---
+++ app/src/main/java/com/example/first_kotlin_app/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.first_kotlin_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            var txtHelloWord = findViewById<TextView>(R.id.txtHelloWord)
+            //Substituir "teste" pelo shared preferences com o nome do usuário, NÃO ESQUEÇA
+            var teste : String? = "Jorge"
+            var nome : String = teste ?: "Bem vindo"
+            if(nome != null) {
+                txtHelloWord.text = "Olá, $nome"
+            } else {
+                txtHelloWord.text = nome
+            }
+            splashAction()
+        }
+    private fun splashAction() {
+        Handler().postDelayed({
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
