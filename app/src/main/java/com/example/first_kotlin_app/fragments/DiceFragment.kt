37dc60d3f2a05beaadfcdb9fd067f5628fe14782
Jorge Lisboa <jorgelisboa659@gmail.com>
2021-07-26 22:11:53 -0300

    Mostrando os números em um AlertDialog, mudança do método 'sorteioDado'.

--- app/src/main/java/com/example/first_kotlin_app/fragments/DiceFragment.kt
+++ app/src/main/java/com/example/first_kotlin_app/fragments/DiceFragment.kt
@@ -1,17 +1,16 @@
 package com.example.first_kotlin_app.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.first_kotlin_app.MenuActivity
 import com.example.first_kotlin_app.R
 import kotlinx.android.synthetic.main.fragment_dice.*
 import kotlin.random.Random
 
-/**
- * A simple [Fragment] subclass.
- */
 class DiceFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,26 +23,28 @@ class DiceFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+    //Eventos dos dados
         dice_4.setOnClickListener {
-            dice_4.text = sorteioDado(4).toString()
+            sorteioDado(4).toString()
         }
         dice_6.setOnClickListener {
-            dice_6.text = sorteioDado(6).toString()
+            sorteioDado(6).toString()
         }
         dice_8.setOnClickListener {
-            dice_8.text = sorteioDado(8).toString()
+            sorteioDado(8).toString()
         }
         dice_10.setOnClickListener {
-            dice_10.text = sorteioDado(10).toString()
+            sorteioDado(10).toString()
         }
         dice_12.setOnClickListener {
-            dice_12.text = sorteioDado(12).toString()
+            sorteioDado(12).toString()
         }
     }
 
-
-    private fun sorteioDado(num : Int): Int {
-        return Random.nextInt(num) + 1
+    private fun sorteioDado(num : Int){
+        val alertDialog = AlertDialog.Builder(activity) // This needs the activity's context
+        var num = Random.nextInt(num) + 1
+        alertDialog.setMessage(num.toString())
+        alertDialog.show()
     }
 }
